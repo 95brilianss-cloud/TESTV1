f3ca89a4a9936e1ffa38aa5e95edf1c6ec66bae9
95brilianss-cloud <95brilianss@gmail.com>
2026-03-12 11:10:56 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Optimasi: Bypass Cache untuk API Google Script
  */
 
-const APP_VERSION = 'v16'; // Update versi setiap ada perubahan file statis
+const APP_VERSION = 'v17'; // Update versi setiap ada perubahan file statis
 const CACHE_NAME = `turbine-logsheet-${APP_VERSION}`;
 
 const assets = [
